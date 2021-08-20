e79fa492fb2c78d7f38e9b7d0d97c3192e4153d8
kasuta96 <kasuta96@gmail.com>
2021-08-20 14:02:38 +0900

    edit trans block render

--- components/Article/BlocksRender/renderers/trans/index.jsx
+++ components/Article/BlocksRender/renderers/trans/index.jsx
@@ -32,33 +32,32 @@ const TransOutput = ({ data, key, count }) => {
   const Tabs = ({ id = "", original, translation }) => {
     const [openTrans, setOpenTrans] = React.useState(false)
     return (
-      <div className="my-6">
-        <a
-          className={
-            "px-5 py-3 shadow-sm rounded block " +
-            (openTrans == true
-              ? "text-white bg-gray-600"
-              : "text-gray-800 bg-gray-50"
-            )
-          }
-          onClick={e => {
-            e.preventDefault()
-            setOpenTrans(prevOpenTrans => !prevOpenTrans)
-          }}
-          href={"#trans" + id}
-        >
-          <svg width="20" height="20" className="float-right text-indigo-300" fill="currentColor" viewBox="0 0 469.333 469.333" xmlns="http://www.w3.org/2000/svg">
+      <div
+        className={
+          "my-6 px-5 py-3 shadow rounded block cursor-pointer " +
+          (openTrans == true
+            ? "text-white bg-gray-600"
+            : "text-gray-800 bg-gray-50"
+          )
+        }
+        onClick={() => {
+          setOpenTrans(prevOpenTrans => !prevOpenTrans)
+        }}
+        data-trans={id}
+      >
+        <div className="float-right text-indigo-300 pl-2">
+          <svg width="20" height="20" fill="currentColor" viewBox="0 0 469.333 469.333" xmlns="http://www.w3.org/2000/svg">
             <path d="M253.227,300.267L253.227,300.267L199.04,246.72l0.64-0.64c37.12-41.387,63.573-88.96,79.147-139.307h62.507V64H192V21.333h-42.667V64H0v42.453h238.293c-14.4,41.173-36.907,80.213-67.627,114.347c-19.84-22.08-36.267-46.08-49.28-71.467H78.72c15.573,34.773,36.907,67.627,63.573,97.28l-108.48,107.2L64,384l106.667-106.667l66.347,66.347L253.227,300.267z"></path>
             <path d="M373.333,192h-42.667l-96,256h42.667l24-64h101.333l24,64h42.667L373.333,192z M317.333,341.333L352,248.853l34.667,92.48H317.333z"></path>
           </svg>
+        </div>
 
-          <div className={openTrans == false ? 'block' : 'hidden'}>
-            <Tag data={original} />
-          </div>
-          <div className={openTrans == true ? 'block' : 'hidden'}>
-            <Tag data={translation} />
-          </div>
-        </a>
+        <div data-lang="orig" className={openTrans == false ? 'block' : 'hidden'}>
+          <Tag data={original} />
+        </div>
+        <div data-lang="tran" className={openTrans == true ? 'block' : 'hidden'}>
+          <Tag data={translation} />
+        </div>
       </div>
     )
   }
