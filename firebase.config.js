f939d7474f0c0164dcc48ce19faa17440e87b458
kasuta96 <kasuta96@gmail.com>
2021-09-14 13:15:56 +0900

    change firebase config file

---
+++ firebase.config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: "project-4347808024585643328.firebaseapp.com",
+  projectId: "project-4347808024585643328",
+  storageBucket: "project-4347808024585643328.appspot.com",
+  messagingSenderId: "832141825649",
+  appId: "1:832141825649:web:17b94b3fe660acd3deedd0",
+  measurementId: "G-XJKEXPWXJR",
+}
+export default firebaseConfig
